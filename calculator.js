25d420e604c7ca9bcf2e9367bf70130819a66cfc
Devansh <devansh.jagatiya@simformsolutions.com>
2026-02-13 18:38:40 +0530

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+
+const display = document.getElementById("displayText")
+document.querySelectorAll(".col").forEach(col => {
+    col.addEventListener("click", function () {
+        console.log(this.dataset.value)
+        display.value+=this.dataset.value
+        // document.getElementById("displayText").innerText = value
+        
+    })
+})
+
+const submit = document.getElementById("submit")
+submit.addEventListener("click" , function(){
+    const expression = display.value
+    console.log(expression)
+
+})
+
